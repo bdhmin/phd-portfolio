0ac7d28e9bf3576f28f839bfcd23ce4c6b1733aa
bdhmin <bryandhmin@gmail.com>
2025-02-06 08:33:55 -0800

    .

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -33,12 +33,12 @@ export default function About() {
           </div>
           <p>
             My goal is to achieve <b>malleable interfaces</b>—ones where
-            end-users can easily, expressively, and broadly customize, without
-            complex code or searching for settings in a bloated settings panel.
-            I explore novel interaction techniques in foundational design
-            patterns that give end-users this control to customize their
-            interface. These techniques could either be supported by direct
-            manipulation or natural language queries to AI.
+            end-users can easily, expressively, and broadly customize without
+            complex code or a bloated settings panel. I explore novel
+            interaction techniques in foundational design patterns that give
+            end-users this control to customize their interface. These
+            techniques could either be supported by direct manipulation or
+            natural language queries to AI.
           </p>
           <p>
             You can reach me via email:{' '}
