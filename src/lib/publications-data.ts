da247640d0411b2570f88e4aaa0b39d4afed9c7f
bdhmin <bryandhmin@gmail.com>
2026-07-21 10:20:40 -0700

    Fixed name typo

--- src/lib/publications-data.ts
+++ src/lib/publications-data.ts
@@ -20,7 +20,7 @@ export const papers: Paper[] = [
     title: "Information Architecture as a Shared Language for Generating Aligned and Persistent User Interfaces",
     subtitle: '',
     type: 'publication',
-    authors: ['Eunhye Kim', 'Daeun Choi', 'Bryan Min', 'Hyunjung Yi', 'Yue Jiang', 'Juho Kim'],
+    authors: ['Eunhye Kim', 'DaEun Choi', 'Bryan Min', 'Hyunjung Yi', 'Yue Jiang', 'Juho Kim'],
     venue: 'UIST 2026 (To appear)',
     resources: [
       // {
